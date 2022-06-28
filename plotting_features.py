65d8992c5f706ec988addea5cb424aaead666e5c
GuySassy <guysassy11@gmail.com>
2022-06-29 00:32:52 +0300

    added another parameters for side calculations

--- plotting_features.py
+++ plotting_features.py
@@ -1,12 +1,7 @@
 from radial_basis_function_extractor import RadialBasisFunctionExtractor
 from mountain_car_with_data_collection import MountainCarWithResetEnv
-import math
 
 import numpy as np
-
-import gym
-from gym import spaces
-from gym.utils import seeding
 import matplotlib.pyplot as plt
 
 
@@ -23,10 +18,15 @@ if __name__ == "__main__":
     # mu_v = (av + bv) / 2
     # sigma_p = (bp - ap) / np.sqrt(12)
     # sigma_v = (bv - av) / np.sqrt(12)
-    minPos = -2.5
-    maxPos = -1
-    maxSpeed = -1
-    minSpeed = -2.5
+
+    minPos = model.min_position
+    maxPos = model.max_position
+    maxSpeed = model.max_speed
+    minSpeed = -model.max_speed
+    # minPos = -2.5
+    # maxPos = -1
+    # maxSpeed = -1
+    # minSpeed = -2.5
     Res = 1000
     posDiscrete = np.linspace(minPos, maxPos, Res)
     speedDiscrete = np.linspace(minSpeed, maxSpeed, Res)
@@ -39,7 +39,6 @@ if __name__ == "__main__":
     states = np.array(states)
     # states = states.T
     features = RBF.encode_states_with_radial_basis_functions(states)
-    k = 0
     feature1_plot = np.reshape(features[:, 0], (Res, Res))
     feature2_plot= np.reshape(features[:, 1], (Res, Res))
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
